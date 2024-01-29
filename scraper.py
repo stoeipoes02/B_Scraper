d8f1db5292cf6a360172196cceabbe2ef49b2639
kick <93348458+stoeipoes02@users.noreply.github.com>
2024-01-29 18:56:52 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 # scraping from https://realpython.github.io/fake-jobs/
 import requests
 from bs4 import BeautifulSoup
+import mariadb
+import sys
+
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
@@ -13,7 +16,21 @@ for job_element in job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())  
-    print(company_element.text.strip()) # only prints the text 
+    print(title_element.text.strip())
+    print(company_element.text.strip()) # only prints the text
     print(location_element.text.strip()) # strips text of unnessecary whitespaces
     print()
+
+try:
+  conn = mariadb.connect(
+    user='exampleuser',
+    password='kick',
+    host='localhost',
+    database='users'
+  )
+
+except mariadb.Error as e:
+  print(f'Error connecting to mariadb platform: {e}')
+  sys.exit(1)
+
+cur = conn.cursor()
