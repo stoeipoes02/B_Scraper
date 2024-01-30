218765aeaa8246c59eef09ab89fd5cb68b11a728
kick <93348458+stoeipoes02@users.noreply.github.com>
2024-01-30 14:14:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     echo 'Connection error: ' . mysqli_connect_error();
   }
 
+
+
 $sql = 'SELECT * FROM users';
 $result = mysqli_query($conn, $sql);
 
@@ -30,7 +32,32 @@ if ($result) {
     echo 'Error: ' . mysqli_error($conn);
 }
 
+
+$sql_jobs = 'SELECT * FROM jobs';
+$result_jobs = mysqli_query($conn, $sql_jobs);
+
+if ($result_jobs) {
+    echo '<h2>Job Information</h2>';
+    echo '<table border="1">';
+    echo '<tr><th>ID</th><th>Title</th><th>Company</th><th>Location</th></tr>';
+
+    while ($row = mysqli_fetch_assoc($result_jobs)) {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['title'] . '</td>';
+        echo '<td>' . $row['company'] . '</td>';
+        echo '<td>' . $row['location'] . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+} else {
+    echo 'Error fetching job information: ' . mysqli_error($conn);
+}
+
 mysqli_close($conn);
+ob_end_flush();
+
 ?>
 
 
@@ -40,6 +67,6 @@ mysqli_close($conn);
     <title>User Information</title>
 </head>
 <body>
-    <h1>User Information</h1>
+    <h1>Information</h1>
 </body>
 </html>
