d24c0c900485a9ab8a3cb914586efd029811bcfb
kick <kickbuur@gmail.com>
2024-01-31 09:12:08 +0100

    message + foto send

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 
 import telegram
+from telegram.constants import ParseMode
 #from telegram.ext import Updater, MessageHandler, filters, CommandHandler
 
 # Load Key
@@ -12,4 +13,7 @@ bot = telegram.Bot(api_key)
 
 
 import asyncio
-asyncio.run(bot.send_message(-4150084167, 'hey'))
+# asyncio.run(bot.send_message(chat_id=-4150084167, text='<b>hallo</b>', parse_mode=ParseMode.HTML))
+
+with open('Yggdrasil.png', 'rb') as picture:
+    asyncio.run(bot.send_photo(chat_id=-4150084167, photo=picture, caption='<b>Foto</b>',parse_mode=ParseMode.HTML))
