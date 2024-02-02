7042352b15549688520b8786aed862c70d9b021f
kick <kickbuur@gmail.com>
2024-02-02 18:03:22 +0100

    working bot.py code which polls

--- bot.py
+++ bot.py
@@ -16,18 +16,6 @@ USERNAME = "Scrappy_Scraper_Bot"
 
 bot = telegram.Bot(token=TOKEN)
 
-# async def main():
-#     count = await bot.get_chat_member_count(chat_id=-4150084167)
-#     print(count)
-
-# asyncio.run(main())
-
-
-#asyncio.run(bot.send_message(chat_id=-4150084167, text='<b>hallo</b>', parse_mode=ParseMode.HTML))
-
-# with open('Yggdrasil.png', 'rb') as picture:
-#     asyncio.run(bot.send_photo(chat_id=-4150084167, photo=picture, caption='<b>Foto</b>',parse_mode=ParseMode.HTML))
-
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('hey i am a banana')
@@ -45,7 +33,7 @@ def handle_response(text: str) -> str:
     if 'hello' in text:
         return 'hey'
     if 'how' in text:
-        return 'IDK'
+        return 'Nibbas'
     return 'i do not understand you..'
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
